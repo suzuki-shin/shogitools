75356aefa412f27c1b1af8cd4794c26a821480b6
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-07-09 19:26:58 +0900

    testでDLしてみた

--- Game/Shogi/Shogiwars.hs
+++ Game/Shogi/Shogiwars.hs
@@ -3,11 +3,25 @@ module Game.Shogi.Shogiwars (
 
 import Text.XML.HXT.Core
 import Text.HandsomeSoup
+import System.FilePath.Posix ((</>))
 import Data.List (nub, sort, isPrefixOf, isSuffixOf)
+import Network.HTTP.Conduit (simpleHttp)
+import Control.Monad (forM_, mapM_)
+import Data.ByteString.Lazy.Char8 as BL8 (writeFile)
 
--- | 
+baseUrl = "http://swks.sakura.ne.jp/wars/"
+kifuSearchUrl = baseUrl </> "kifusearch/"
+
+-- | userIdを受け取って、そのユーザーのkifファイルをダウンロードして、DBにurl等を保存する
 downloadKifs :: String -> IO ()
-downloadKifs user = undefined
+downloadKifs user = do
+  urls <- kifUrls user
+  forM_ (zip urls [0..]) $ \(u, i) -> do
+    kif <- simpleHttp u
+    print kif
+    BL8.writeFile ("k" ++ show i) kif
+  return ()
+
 
 -- | userIdを受け取ってkifファイルの URLのリストを返す
 kifUrls :: String -> IO [String]
@@ -17,5 +31,5 @@ kifUrls user = do
 --   let doc = readString [withParseHTML yes, withWarnings no] $ BL8.unpack c
   let doc = readString [withParseHTML yes, withWarnings no] c
   links <- runX $ doc //> css "div" >>> hasAttrValue "id" (== "wrap3") >>> css "a" ! "href"
-  return $ sort $ nub $ filter (isSuffixOf ".kif") links
+  return $ map ((baseUrl ++) . dropWhile (=='.')) $ sort $ nub $ filter (isSuffixOf ".kif") links
 
