4d907576e2cc6837062e3daab3530611851d3ccf
Shinichiro Suzuki <shinichiro.su@gmail.com>
2014-07-10 20:06:09 +0900

    user指定してDLまでできるようになった

--- Game/Shogi/Shogiwars.hs
+++ Game/Shogi/Shogiwars.hs
@@ -11,7 +11,7 @@ import Network.HTTP.Conduit
 import Control.Monad (forM_, mapM_)
 import Control.Applicative ((<$>))
 import Data.ByteString.Lazy.Char8 as BL8 (writeFile, unpack, pack)
-import Data.ByteString.Char8 as B8 (writeFile, unpack, pack)
+import Data.ByteString.Char8 as B8 (writeFile, unpack, pack, ByteString)
 import Data.Conduit
 import qualified Data.Conduit.Binary as CB
 import Control.Monad.IO.Class (liftIO)
@@ -25,18 +25,18 @@ baseUrl = "http://swks.sakura.ne.jp/wars/"
 kifuSearchUrl = baseUrl </> "kifusearch/"
 
 -- | userIdを受け取って、そのユーザーのkifファイルをダウンロードして、DBにurl等を保存する
-downloadKifs :: String -> IO ()
+downloadKifs :: ByteString -> IO ()
 downloadKifs user = do
   urls <- kifUrls user
   forM_ (zip urls [0..]) $ \(u, i) -> do
     kif <- simpleHttp u
     print kif
-    BL8.writeFile ("k" ++ show i) kif
+    BL8.writeFile ((B8.unpack user) ++ show i) kif
   return ()
 
 
 -- | userIdを受け取ってkifファイルの URLのリストを返す
-kifUrls :: String -> IO [String]
+kifUrls :: ByteString -> IO [String]
 kifUrls user = do
 --   c <- simpleHttp url
   kifusearchResultPage user
@@ -48,7 +48,7 @@ kifUrls user = do
   return $ map ((baseUrl ++) . dropWhile (=='.')) $ sort $ nub $ filter (isSuffixOf ".kif") links
 
 
--- | 将棋ウォーズ棋譜検索ベータの検索結果ページを返す
+-- 将棋ウォーズ棋譜検索ベータの検索結果ページを返す
 -- kifusearchResultPage :: String -> IO String
 kifusearchResultPage user = runResourceT $ do
 --   manager <- liftIO $ newManager def
@@ -57,7 +57,9 @@ kifusearchResultPage user = runResourceT $ do
 --   let postRequest = urlEncodedBody [("keyword1", "soradayo"),("csrfmiddlewaretoken","ubK8vuZNfZbBsBvlIOpFrDsBcRIGHtwg")] req
   params <- liftIO $ kifusearchPageInputParams
   liftIO $ print params
-  let params2 = map ((\(a,b) -> (B8.pack a, B8.pack b)) . (\(k,v) -> if k == "name1" then (k,user) else (k,v))) $ params
+--   let params2 = map ((\(a,b) -> (B8.pack a, B8.pack b)) . (\(k,v) -> if k == "name1" then (k,user) else (k,v))) $ params
+  let params2 :: [(B8.ByteString, B8.ByteString)]
+      params2 = [("name1", user), ("csrfmiddlewaretoken", B8.pack $ fromJust $ lookup "csrfmiddlewaretoken" params), ("gtype", "0")]
       req = req' { cookieJar = Just $ createCookieJar [cookie params2] }
       postRequest = urlEncodedBody params2 req
 --   let params2 = map (\(k,v) -> if k == "name1" then (k,user) else (k,v)) params
@@ -68,10 +70,10 @@ kifusearchResultPage user = runResourceT $ do
   responseBody response $$+- CB.sinkFile $ "DL" </> "result.html"
 --   responseBody response $$ CB.sinkHandle stdout
   where
-    cookie_value params = fromJust $ lookup "csrfmiddlewaretoken" params
+--     cookie_value params = 
 --     cookie :: Cookie
     cookie params = Cookie { cookie_name = "csrftoken"
-                     , cookie_value = cookie_value params
+                     , cookie_value = fromJust $ lookup "csrfmiddlewaretoken" params
                      , cookie_path = "/"
                  , cookie_expiry_time = future
                  , cookie_domain = "swks.sakura.ne.jp"
